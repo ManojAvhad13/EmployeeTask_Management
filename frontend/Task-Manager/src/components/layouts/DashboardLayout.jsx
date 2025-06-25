49b69d96317eb993129dfd7b991844ab29da1f41
ManojAvhad13 <manojavhad05@gmail.com>
2025-06-25 16:14:46 +0530

    commit

--- frontend/Task-Manager/src/components/layouts/DashboardLayout.jsx
+++ frontend/Task-Manager/src/components/layouts/DashboardLayout.jsx
@@ -2,33 +2,10 @@ import React, { useContext, useState } from 'react'
 import { UserContext } from '../context/userContext'
 import Navbar from './Navbar';
 import SideMenu from './SideMenu';
-import { useNavigate } from 'react-router-dom';
-import axiosInstance from '../../utils/axiosInstance';
-import { API_PATHS } from '../../utils/apiPaths';
 
 const DashboardLayout = ({ children, activeMenu }) => {
 
     const { user } = useContext(UserContext);
-
-    const navigate = useNavigate();
-
-    const [dashboardData, setDashboardData] = useState(null);
-    const [pieChartData, setPieChartData] = useState([]);
-    const [barChartData, setBarChartData] = useState([]);
-
-    const getDashboardData = async () => {
-        try {
-            const response = await axiosInstance.get(
-                API_PATHS.TASK.GET_DASHBOARD_DATA
-            );
-
-            if (response.data) {
-                setDashboardData(response.data);
-            }
-        } catch (error) {
-
-        }
-    }
 
     return (
         <div className=''>
